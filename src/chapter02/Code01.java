67c31310394c4e232a381cd3ad0e50d2996824cf
tomines-jaeyoung <tominesss12@gmail.com>
2025-03-17 11:52:35 +0900

    main method 사용법, 연결연산자, 덧셈연산자, pintln, print차이점

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("AIsoftware");
+        System.out.println("Jaeyoungkim");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:"+ (100 + 200));
+        System.out.println(100 + 200 + "won");}
+}
+
