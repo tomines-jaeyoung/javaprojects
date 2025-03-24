e6371ed3e1d88180038fc5bd9c4b181c85178ee3
tomines-jaeyoung <tominesss12@gmail.com>
2025-03-24 10:44:01 +0900

    사칙계산기

---
+++ src/chapter02/code02_07.java
@@ -0,0 +1,31 @@
+package chapter02;
+
+public class code02_07 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 50;
+
+        int result = num1 + num2;
+        System.out.printf("%d + %d = %d\n",num1, num2, result);
+
+        result = num1 - num2;
+        System.out.printf("%d - %d = %d\n",num1, num2, result);
+
+        result = num1 * num2;
+        System.out.printf("%d * %d = %d\n",num1, num2, result);
+
+        result = num1 / num2;
+        System.out.printf("%d / %d = %d\n",num1, num2, result);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
