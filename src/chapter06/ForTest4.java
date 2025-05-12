18e4708055f3e614672dd123ce5c510125068a76
tomines-jaeyoung <tominesss12@gmail.com>
2025-05-12 10:35:00 +0900

    6장 반복문 = For4, 홀수의 합계를 구함

--- src/chapter06/ForTest4.java
+++ src/chapter06/ForTest4.java
@@ -3,12 +3,15 @@ package chapter06;
 public class ForTest4 {
 
     public static void main(String[] args) {
-int n = 100;
+int n = 1000;
         int sum=0;
+        String oddEven = "";
         for(int i =1; i<=n; i++) {
-            sum += i;
-        }
-        System.out.println("1부터 " + n + " 까지의 합계는 " + sum + " 입니다");
+            if(i % 2 == 0){
+                sum += i;
+                oddEven ="홀수";
+            }}
+        System.out.println("1부터 " + n + " 까지의 "+oddEven+"합계는 " + sum + " 입니다");
 
     }
 }
