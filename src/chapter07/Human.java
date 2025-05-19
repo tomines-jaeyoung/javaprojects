c16d2a70276e34dbc59e02cc9fc248063b57333a
tomines-jaeyoung <tominesss12@gmail.com>
2025-05-19 11:48:17 +0900

    7장 클래스와 객체에 관련된 예제

---
+++ src/chapter07/Human.java
@@ -0,0 +1,18 @@
+package chapter07;
+
+public class Human {
+
+
+    String name;
+    int age;
+
+    public void walk(String place) {
+        System.out.println(name + "이(가)" + place + " 까지 걷는다.");
+    }
+public void eat(String food){
+    System.out.println(name + "이(가)" + food + "을(를) 먹는다.");
+}
+
+
+
+}
