5c940b6dac6301cbba0f0deea3648486998f16e4
tomines-jaeyoung <tominesss12@gmail.com>
2025-05-19 09:27:19 +0900

    6장 반복문 = While문_무한루프

--- src/chapter06/WhileTest2.java
+++ src/chapter06/WhileTest2.java
@@ -4,7 +4,7 @@ public class WhileTest2 {
 
     public static void main(String[] args) {
         while(true) {
-            System.out.println("AI");
+            System.out.println("AI1");
         }
 
 
